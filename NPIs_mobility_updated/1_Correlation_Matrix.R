c8c779794a8eff3b9b6a4074a0fd7d52b733c1db
EllieBloom <elliebloom94@gmail.com>
2022-07-04 17:33:11 +0100

    converted to standard function

--- NPIs_mobility_updated/1_Correlation_Matrix.R
+++ NPIs_mobility_updated/1_Correlation_Matrix.R
@@ -1,141 +1,214 @@
-# Correlation between mobility measures
+# Creating mobility correlation heatmaps using standard function
+
+# Date started: 3rd July 2022
 
 library(tidyverse)
 
-# Date started: 23rd May 2022
+# Date started: 3rd July 2022
 
+# Data at Gb and county level
 google_overall <- read.csv("/Users/elliebloom/Desktop/Masters/Project/Data/Google/google_overall.csv")
 
 
-# Filterting for GB dataset
-google_gb <- google_overall %>% filter(sub_region_1=="") # Where there is no sub-region, the data is for GB overall
-
+# Data at England and regional level
 google_england <- readRDS("/Users/elliebloom/Desktop/Masters/Project/Analysis/Mapping/Outputs/google_england_wide.rds")
 
-# Looking at correlations between metrics
 
-#  Correlations between metrics at England level (using moving averages)---------
+start_date_default <- min(google_england$date)
+end_date_default <-  max(google_england$date)
 
+# Useful dates
+lockdown_1_start <- as.Date("2020-03-26","%Y-%m-%d")
+lockdown_1_end <- as.Date("2020-06-15","%Y-%m-%d")
 
-google_england_movingav <- google_england %>% filter(region=="ENGLAND")
-# Select moving averages only
+lockdown_2_start <- as.Date("2020-11-05","%Y-%m-%d")
+lockdown_2_end <- as.Date("2020-12-02","%Y-%m-%d")
 
-google_england_movingav <- google_england_movingav[,(10:15)]
+lockdown_3_start <- as.Date("2021-01-06","%Y-%m-%d")
+lockdown_3_end <- as.Date("2021-04-21","%Y-%m-%d")
 
+# Regions
+region_list <- dput(unique(google_england$region))
+region_list_neat <- c("England", "East of England", "East Midlands", "London", "North East", 
+                      "North West", "South East", "South West", "West Midlands", "Yorkshire and the Humber"
+)
 
+# This currently has a default region of England - can't get this to work as a variable?
 
-# Correlation matrix
-# Rename the columns to make visualisation easier
+google_england %>% filter(date>=start_date_default)
 
-
-colnames(google_england_movingav)<-c("Retail","Grocery and Pharmacy","Parks",
-                                      "Transit Stations", "Workplaces", "Residential")
-
-# Remove the NAs due to moving av (1st 3 rows and last 3 rows)
-
-head(google_england_movingav)
-google_england_movingav <- google_england_movingav[-c(1:3),]
-tail(google_england_movingav)
-n = nrow(google_england_movingav)
-google_england_movingav <- google_england_movingav[-c((n-2):n),]
-tail(google_england_movingav)
-
-# Correlation matrix
-cormat <- round(cor(google_england_movingav[,c("Retail","Grocery and Pharmacy","Parks",
-                                 "Transit Stations", "Workplaces", "Residential")]),2)
-# Make long
-
-library(reshape2)
-melted_cormat <- melt(cormat)
-head(melted_cormat)
-
-# Preparing visualisation
-
-# Get lower triangle of the correlation matrix
-get_lower_tri<-function(cormat){
-  cormat[upper.tri(cormat)] <- NA
-  return(cormat)
-}
-# Get upper triangle of the correlation matrix
-get_upper_tri <- function(cormat){
-  cormat[lower.tri(cormat)]<- NA
+# Function to create correlation matrix
+get_cormat <- function(data, region_interest,start_date=start_date_default, end_date=end_date_default){
+  data_filter <- data %>% filter(region==region_interest,date>=start_date, date<=end_date)
+  print("Correlation  matrix is for:")
+  print(unique(data_filter$region))
+  data_movingav <- data_filter[,(10:15)]
+  colnames(data_movingav) <- c("Retail","Grocery and Pharmacy","Parks",
+                               "Transit Stations", "Workplaces", "Residential")
+  data_movingav <- data_movingav[-c(1:3),]
+  n <- nrow(data_movingav)
+  data_movingav <- data_movingav[-c((n-2):n),]  
+  cormat <- round(cor(data_movingav),2)
   return(cormat)
 }
 
-upper_tri <- get_upper_tri(cormat)
-upper_tri
-
-
-# Melt the correlation matrix
-
-melted_cormat <- melt(upper_tri, na.rm = TRUE)
-
-# Heatmap
-
-ggplot(data = melted_cormat, aes(X2, X1, fill = value))+
-  geom_tile(color = "white")+
-  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
-                       midpoint = 0, limit = c(-1,1), space = "Lab", 
-                       name="Pearson\nCorrelation") +
-  theme_minimal()+ 
-  theme(axis.text.x = element_text(angle = 45, vjust = 1, 
-                                   size = 12, hjust = 1),
-        axis.text.y = element_text(size=12))+
-  coord_fixed()
-
-# Reorder the correlation matrix
-reorder_cormat <- function(cormat){
-  # Use correlation between variables as distance
-  dd <- as.dist((1-cormat)/2)
-  hc <- hclust(dd)
-  cormat <-cormat[hc$order, hc$order]
+# Function to return heatmap
+convert_cormat_heatmap <- function(cormat, region_interest_neat,time_period_name){
+  library(reshape2)
+  melted_cormat <- melt(cormat)
+  head(melted_cormat)
+  
+  # Preparing visualisation
+  
+  # Get lower triangle of the correlation matrix
+  get_lower_tri<-function(cormat){
+    cormat[upper.tri(cormat)] <- NA
+    return(cormat)
+  }
+  # Get upper triangle of the correlation matrix
+  get_upper_tri <- function(cormat){
+    cormat[lower.tri(cormat)]<- NA
+    return(cormat)
+  }
+  
+  upper_tri <- get_upper_tri(cormat)
+  upper_tri
+  
+  
+  # Melt the correlation matrix
+  
+  melted_cormat <- melt(upper_tri, na.rm = TRUE)
+  
+  # Heatmap
+  
+  ggplot(data = melted_cormat, aes(X2, X1, fill = value))+
+    geom_tile(color = "white")+
+    scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
+                         midpoint = 0, limit = c(-1,1), space = "Lab", 
+                         name="Pearson\nCorrelation") +
+    theme_minimal()+ 
+    theme(axis.text.x = element_text(angle = 45, vjust = 1, 
+                                     size = 12, hjust = 1),
+          axis.text.y = element_text(size=12))+
+    coord_fixed()
+  
+  # Reorder the correlation matrix
+  reorder_cormat <- function(cormat){
+    # Use correlation between variables as distance
+    dd <- as.dist((1-cormat)/2)
+    hc <- hclust(dd)
+    cormat <-cormat[hc$order, hc$order]
+  }
+  
+  # Reorder the correlation matrix
+  cormat <- reorder_cormat(cormat)
+  upper_tri <- get_upper_tri(cormat)
+  # Melt the correlation matrix
+  melted_cormat <- melt(upper_tri, na.rm = TRUE)
+  # Create a ggheatmap
+  ggheatmap <- ggplot(melted_cormat, aes(X2, X1, fill = value))+
+    geom_tile(color = "white")+
+    scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
+                         midpoint = 0, limit = c(-1,1), space = "Lab", 
+                         name="Pearson\nCorrelation",
+                         na.value="white") +
+    theme_minimal()+ # minimal theme
+    theme(axis.text.x = element_text(angle = 45, vjust = 1, 
+                                     size = 12, hjust = 1),
+          axis.text.y = element_text(size=12),
+          plot.title = element_text(hjust = 0))+
+    coord_fixed()
+  
+  
+  # Add correlation coefficients
+  
+  ggheatmap_final<- ggheatmap + 
+    geom_text(aes(X2, X1, label = value), color = "black", size = 4) +
+    ggtitle(region_interest_neat) +
+    labs(subtitle=time_period_name)+
+    theme(
+      axis.title.x = element_blank(),
+      axis.title.y = element_blank(),
+      panel.grid.major = element_blank(),
+      panel.border = element_blank(),
+      panel.background = element_blank(),
+      axis.ticks = element_blank(),
+      legend.justification = c(1, 0),
+      legend.position = c(0.4, 0.7),
+      legend.direction = "horizontal")+
+    guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
+                                 title.position = "top", title.hjust = 0.5))+
+    scale_y_discrete(position = "right")
+  
+  
+  
+  return(ggheatmap_final)
+  print(ggheatmap_final)
+  
 }
 
-# Reorder the correlation matrix
-cormat <- reorder_cormat(cormat)
-upper_tri <- get_upper_tri(cormat)
-# Melt the correlation matrix
-melted_cormat <- melt(upper_tri, na.rm = TRUE)
-# Create a ggheatmap
-ggheatmap <- ggplot(melted_cormat, aes(X2, X1, fill = value))+
-  geom_tile(color = "white")+
-  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
-                       midpoint = 0, limit = c(-1,1), space = "Lab", 
-                       name="Pearson\nCorrelation",
-                       na.value="white") +
-  theme_minimal()+ # minimal theme
-  theme(axis.text.x = element_text(angle = 45, vjust = 1, 
-                                   size = 12, hjust = 1),
-        axis.text.y = element_text(size=12),
-        plot.title = element_text(hjust = 0.5))+
-  coord_fixed()
-# Print the heatmap
-print(ggheatmap)
-
-# Add correlation coefficients
-
-ggheatmap_final<- ggheatmap + 
-                  geom_text(aes(X2, X1, label = value), color = "black", size = 4) +
-                  ggtitle("Correlation heatmap of Google Mobility at England level") +
-                  theme(
-                    axis.title.x = element_blank(),
-                    axis.title.y = element_blank(),
-                    panel.grid.major = element_blank(),
-                    panel.border = element_blank(),
-                    panel.background = element_blank(),
-                    axis.ticks = element_blank(),
-                    legend.justification = c(1, 0),
-                    legend.position = c(0.4, 0.7),
-                    legend.direction = "horizontal")+
-                  guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
-                                               title.position = "top", title.hjust = 0.5))+
-                  scale_y_discrete(position = "right")
 
 
+# In England
+region_number=1
+cormat<- get_cormat(data=google_england, region_interest=region_list[region_number])
+cormat
+heatmap <- convert_cormat_heatmap(cormat,region_list_neat[region_number],"Whole period") 
+heatmap
 
-ggheatmap_final
-setwd("/Users/elliebloom/Desktop/Masters/Project/Analysis/NPIs_mobility/Outputs")
-ggsave("GB_mobility_heatmap.pdf",ggheatmap_final)
 
-# Now make the heatmap process into a function that can be applied to any region and any set of dates
+# Other regions
+region_number=2
+cormat<- get_cormat(data=google_england, region_interest=region_list[region_number])
+cormat
+heatmap <- convert_cormat_heatmap(cormat,region_list_neat[region_number],"Whole period") 
+heatmap
 
+# Other regions
+region_number=3
+cormat<- get_cormat(data=google_england, region_interest=region_list[region_number])
+cormat
+heatmap <- convert_cormat_heatmap(cormat,region_list_neat[region_number],"Whole period") 
+heatmap
+
+# Other regions
+region_number=4
+cormat<- get_cormat(data=google_england, region_interest=region_list[region_number])
+cormat
+heatmap <- convert_cormat_heatmap(cormat,region_list_neat[region_number],"Whole period") 
+heatmap
+
+# Other regions
+region_number=5
+cormat<- get_cormat(data=google_england, region_interest=region_list[region_number])
+cormat
+heatmap <- convert_cormat_heatmap(cormat,region_list_neat[region_number],"Whole period") 
+heatmap
+
+# Other regions
+region_number=6
+cormat<- get_cormat(data=google_england, region_interest=region_list[region_number])
+cormat
+heatmap <- convert_cormat_heatmap(cormat,region_list_neat[region_number],"Whole period") 
+heatmap
+
+# Other regions
+region_number=7
+cormat<- get_cormat(data=google_england, region_interest=region_list[region_number])
+cormat
+heatmap <- convert_cormat_heatmap(cormat,region_list_neat[region_number],"Whole period") 
+heatmap
+
+# Other regions
+region_number=8
+cormat<- get_cormat(data=google_england, region_interest=region_list[region_number])
+cormat
+heatmap <- convert_cormat_heatmap(cormat,region_list_neat[region_number],"Whole period") 
+heatmap
+
+# Other regions
+region_number=9
+cormat<- get_cormat(data=google_england, region_interest=region_list[region_number])
+cormat
+heatmap <- convert_cormat_heatmap(cormat,region_list_neat[region_number],"Whole period") 
+heatmap
